25fbec1d55bda7e142688b19df1b452c29b5d2f3
Luis Fernando de Andrade <82834641+lafai10@users.noreply.github.com>
2022-12-15 05:37:42 -0300

    a

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "9d48d389e02a8543160af24f905f180c"
+
+let img_url ="https://image.tmdb.org/t/p/w500"
+let generes_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
